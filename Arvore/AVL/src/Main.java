b57d26a05b229e6697dbe35ec4c84e954c80b029
matusilva <matheus10xy@gmail.com>
2020-11-20 20:26:09 -0300

    criando interação com usuário na classe main

--- Arvore/AVL/src/Main.java
+++ Arvore/AVL/src/Main.java
@@ -1,20 +1,51 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static void menu() {
+        System.out.println("| 1 | - Inserir\n| 2 | - Remover\n| 3 | - Buscar\n| 4 | - SAIR");
+        System.out.println("| --- Selecione a Opção que deseja realizar:");
+    }
     public static void main(String[] args) {
         AVL avl = new AVL();
-
-        System.out.println("| ----- Inserindo Valores: 4, 2, 7, 9, 11");
-        avl.inserir(4);
-        avl.inserir(2);
-        avl.inserir(7);
-        avl.inserir(9);
-        avl.inserir(11);
-
-        avl.print();
-
-        System.out.print("| ----- Removendo o valor: ");
         Scanner sc = new Scanner(System.in);
+
+        int op;
+        do {
+            menu();
+            op = sc.nextInt();
+
+            if (op == 1) {
+                System.out.print("Digite o valor que deseja inserir: ");
+                int valor = sc.nextInt();
+                avl.inserir(valor);
+                System.out.println(" --- AVL --- ");
+                avl.print();
+            }
+
+            if (op == 2) {
+                avl.print();
+                System.out.print("Digite o valor que deseja remover: ");
+                int valor = sc.nextInt();
+                NoAVL find = (NoAVL) avl.buscar(valor, avl.getRaiz());
+                avl.remover(find);
+                avl.print();
+            }
+
+            if (op == 3) {
+                avl.print();
+                System.out.print("Digite o valor que deseja buscar ");
+                int valor = sc.nextInt();
+                NoAVL find = (NoAVL) avl.buscar(valor, avl.getRaiz());
+                System.out.println(find.getO());
+            }
+
+        } while (op != 4);{
+            sc.close();
+            System.exit(0);
+        }
+
+
         int valor = sc.nextInt();
         NoAVL find = (NoAVL) avl.buscar(valor, avl.getRaiz());
         avl.remover(find);
